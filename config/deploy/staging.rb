dcc1e396894a151e4f9d89ab4e6f0a3c8dd9ca89
anjankumarhn <anjankumar.h@sigmainfo.net>
2017-05-04 10:28:47 +0530

    Initial Commit

---
+++ config/deploy/staging.rb
@@ -0,0 +1,22 @@
+set :stage, :staging
+set :unicorn_rack_env, 'staging'
+set :branch, :master
+set :deploy_to, '/u01/apps/sigma/deploy1/'
+set :log_level, :debug
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+role :app, %w{deploy@192.168.4.34}
+role :web, %w{deploy@192.168.4.34}
+role :db, %w{deploy@192.168.4.34}
+server '192.168.4.34', roles: %w{:web, :app, :db}, user: 'deploy'
+
+set :ssh_options, {
+   #verbose: :debug,
+   keys: %w(~/.ssh/id_rsa),
+   auth_methods: %w(publickey)
+}
